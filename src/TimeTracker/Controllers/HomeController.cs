5303392e3ce4fbde003814a200f156f9b9b64a9d
David Gouge <davegouge@gmail.com>
2016-01-28 10:57:27 +0000

    First stab at inserting Time Log data from form.

--- src/TimeTracker/Controllers/HomeController.cs
+++ src/TimeTracker/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 using TimeTracker.Model;
 
 namespace TimeTracker.Controllers
@@ -15,7 +16,22 @@ namespace TimeTracker.Controllers
 
         public IActionResult Index()
         {
-            var timelogs = _dataContext.TimeLogs.OrderBy(tl => tl.Start).ToList();
+            var timelogs = _dataContext.TimeLogs.Include(log => log.Client).OrderBy(tl => tl.Start).ToList();
+            return View();
+        }
+
+        public IActionResult AddLog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddLog(TimeLog log)
+        {
+            log.ClientId = 1;
+            log.UserId = 1;
+            _dataContext.TimeLogs.Add(log);
+            _dataContext.SaveChanges();
             return View();
         }
     }
